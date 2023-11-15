fc69e992577d66df47c80ebab12448e576674afd
rawrmonster17 <108893772+rawrmonster17@users.noreply.github.com>
2023-11-14 19:29:02 -0600

    update (#9)
    modified the check for reboot required method in computer

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@
 import pipeline
 import onepasswordcli
 import computer
-import json
 
 
 if __name__ == '__main__':
     op_manager = onepasswordcli.OnePasswordManager()
     result = op_manager.run_op_command(["item", "get", "server01_ssh"])
     result = op_manager.parse_user_password_website(result)
+    # This will end up being a docker-compose.yml file
     file_to_send = "src/test.txt"
     remote_path = "/home/rawrmonster/test.txt"
     pipelineobj = pipeline.SSHClient(result["website"],
@@ -17,8 +17,11 @@ if __name__ == '__main__':
     pipelineobj.send_file(file_to_send, remote_path)
     computerobj = computer.Computer(pipelineobj)
     output = computerobj.check_for_reboot_required()
-    json_output = json.loads(output)
-    if str(json_output["stdout"]).strip().lower() == "true":
-        print("reboot required")
+    if output:
+        print("Reboot required")
+        # I want to add the reboot here but not yet.
+        # computerobj.reboot_if_required()
+    else:
+        pass
     output = computerobj.check_apt_upgrades()
     print(output)
